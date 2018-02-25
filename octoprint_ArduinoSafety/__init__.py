77468cdcfae2592b4b4a4017eeff9c782daef324
newkev <newkev@web.de>
2018-02-25 13:17:12 +0100

    Test4

--- octoprint_ArduinoSafety/__init__.py
+++ octoprint_ArduinoSafety/__init__.py
@@ -4,9 +4,19 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
-    def on_after_startup(self):
-        self._logger.info("Hello World!")
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
+def on_after_startup(self):
+    self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+def get_settings_defaults(self):
+    return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+
+def get_template_configs(self):
+    return [
+        dict(type="navbar", custom_bindings=False),
+        dict(type="settings", custom_bindings=False)
+    ]
 
 __plugin_name__ = "Hello World"
 __plugin_implementation__ = HelloWorldPlugin()
